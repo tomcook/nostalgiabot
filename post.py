31a0cdc7dc4805b51fec8e60c9e25762339d2aeb
Tom Cook <tomcook@users.noreply.github.com>
2020-10-23 07:41:50 -0700

    State Storage
    Use a GitHub Gist to store some state information for all the photos we've already posted to Twitter for later de-duping.

--- post.py
+++ post.py
@@ -1,16 +1,27 @@
 import os
 import json
+import github
 import shutil
 import random
 import requests
+from datetime import datetime
 from TwitterAPI import TwitterAPI
 
+stateDbGist = 'd34ccb38adf1b9ec9366b892b3e40ae6'
 memoryDbUrl = 'https://raw.githubusercontent.com/tomcook/nostalgiabot-obama/main/memories.json'
+
+githubToken = os.environ.get('GIST_TOKEN')
 
 api = TwitterAPI(os.environ.get('TWITTER_CONSUMER_KEY'), 
                  os.environ.get('TWITTER_CONSUMER_SECRET'),
                  os.environ.get('TWITTER_ACCESS_TOKEN_KEY'),
                  os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'))
+
+# Get the GitHub Gist that contains our state database
+gh = github.Github(githubToken)
+gist = gh.get_gist(stateDbGist)
+
+stateDb = json.loads(gist.files['state.json'].content)
 
 # Get the memories DB
 
@@ -48,4 +59,13 @@ print('UPLOAD MEDIA SUCCESS' if r.status_code == 200 else 'UPLOAD MEDIA FAILURE:
 if r.status_code == 200:
     media_id = r.json()['media_id']
     r = api.request('statuses/update', {'status': tweet, 'media_ids': media_id})
-    print('UPDATE STATUS SUCCESS' if r.status_code == 200 else 'UPDATE STATUS FAILURE: ' + r.text)
+    if r.status_code == 200 
+        print('UPDATE STATUS SUCCESS')
+
+        # Append to the state database
+
+        stateDb[chosenMemory['title']] = {"tweet_id":r.text['id'], "posted_on":r.text['created_at']}
+
+        gist.edit(files={"state.json": github.InputFileContent(content=json.dumps(stateDb))})
+    else:
+        print('UPDATE STATUS FAILURE: ' + r.text)

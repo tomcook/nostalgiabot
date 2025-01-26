700bfcfb5dad6072a9d9b426f38e13f58f73483f
Tom Cook <819843+tomcook@users.noreply.github.com>
2025-01-26 22:19:00 +0800

    Actually update the script with the new workflow

--- post-bluesky.py
+++ post-bluesky.py
@@ -1,16 +1,13 @@
 import os
 import json
 import github
-import shutil
 import regex
-import random
 import pprint
 import requests
 from atproto import Client
 from atproto import client_utils
 from pprint import pprint
 from datetime import datetime
-from PIL import Image
 
 api = Client()
 api.login(os.environ.get('BLUESKY_USERNAME'), os.environ.get('BLUESKY_PASSWORD'))
@@ -22,47 +19,6 @@ gist = gh.get_gist(os.environ.get('STATE_DB_GIST'))
 stateDb = json.loads(gist.files['state-bluesky.json'].content)
 
 print(f" : Loaded state DB with {len(stateDb)} entries")
-
-# Get the memories DB
-
-req = requests.get(os.environ.get('MEMORY_DB_URL'))
-memories = req.json()
-
-print(f" : Loaded memories DB with {len(memories)} memories")
-
-# Randomly select memories from the database, comparing them to the state database
-#   to make sure they haven't already been posted. If they have then pick a new one.
-
-print(" : Choosing a random memory")
-
-while True:
-    # Find our random memory to post from the DB
-
-    chosenMemory = random.choice(memories)
-
-    print(f" : Checking memory '{chosenMemory['title']}'")
-
-    if chosenMemory['title'] in stateDb:
-        print(f" : WARNING: Memory {chosenMemory['title']} already posted; choosing new one...")
-        continue
-    else:
-        break
-
-print(" : Memory Chosen")
-print("==================================================================")
-pprint(chosenMemory)
-print("==================================================================")
-
-# Download the memory image
-
-response = requests.get(chosenMemory['source'], stream=True)
-
-with open('img.jpg', 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
-
-# Resize image
-img = Image.open('img.jpg')
-img.save('img_resized.jpg', format="JPEG", quality=50)
 
 # Assemble the skeet text
 
